03b1994fc913ee834d82a699a55507542e2bd293
stagebo <1254117589@qq.com>
2018-03-15 09:56:10 +0800

    finish 1.0.0

--- web/handler_history.py
+++ web/handler_history.py
@@ -28,26 +28,35 @@ class HistoryHandler(pyrestful.rest.RestHandler):
     @get(_path="/history/get_fivemin_data",_produces=mediatypes.APPLICATION_JSON)
     def get_fivemin_data(self):
         try:
+            time_mark = self.get_argument("time",None)
+            if not time_mark:
+                time_mark = datetime.datetime.now().strftime("%Y-%m-%d %H")
+            time_stamp = time.strptime(time_mark,"%Y-%m-%d %H")
+            timestart = int(time.mktime(time_stamp))*1000
+            timeend = timestart + 60*60*1000
             device_id = int(self.get_argument("id",None))
             if not device_id:
                 return {"ret":0,"msg":"id不能为空"}
-            now = int(round(time.time() * 1000))
-            target = now - 5*1000
-            str_target =str(target)
+
+
+
             condition = {
                 'id':device_id,
                 'time':{
-                    "$gte":str_target
+                    "$gte":str(timestart),
+                    "$lte":str(timeend)
                 }
             }
-            print(str_target)
+            # print(condition)
 
             ret = db.db.device.find(condition)
             retdata = []
             for row in ret:
                 del row["_id"]
                 retdata.append(row)
+            # print(retdata)
             return retdata
         except:
+
             traceback.print_exc()
 
